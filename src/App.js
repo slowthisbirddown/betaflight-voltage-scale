a25e006f22bd6874ee31444722f93893ebf53a23
Adam Yaccarino <hupo1989@gmail.com>
2021-04-28 13:17:05 -0600

    draw the rest of the fucking owl

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  // Create Hooks
+  const [old, setOld] = useState(0)
+  const [mah, setMah] = useState(1500)
+  const [osd, setOsd] = useState(1700)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <label>mAH in OSD</label>
+        <input id="old" type="text" value={osd} onChange={(e) => setOsd(e.target.value)}></input>
+      </div>
+
+      <div>
+        <label>mAH replaced</label>
+        <input id="mah" type="text" value={mah} onChange={(e) => setMah(e.target.value)}></input>
+      </div>
+
+      <div>
+        <label>Old scale</label>
+        <input id="mah" type="text" value={old} onChange={(e) => setOld(e.target.value)}></input>
+      </div>
+
+      <label>New scale</label>
+      <input id="new" disabled="true" type="text" value={osd / mah * old}></input>
     </div>
   );
 }
